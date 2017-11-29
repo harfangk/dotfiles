6b21c2bcd68bb2f64595437c284db04eb2382fb0
Susensio <susensio@gmail.com>
2017-11-29 14:10:01 +0100

    Fix stderr redirect

--- config-install.sh
+++ config-install.sh
@@ -2,14 +2,14 @@ git clone --bare https://www.github.com/harfangk/dotfiles.git $HOME/.dotfiles
 function dotfiles {
   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
+
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
 else
   echo "Backing up pre-existing dot files.";
-dotfiles checkout 2>$1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {}
-  .dotfiles-backup/{}
+  mkdir -p .dotfiles-backup
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
